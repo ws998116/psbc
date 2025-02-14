f3f73663ba558bd1058895d7dad708dd110e8452
Wyatt Smith <ws998116@gmail.com>
2025-02-14 14:08:53 -0500

    animation

--- src/components/SeriesCard.tsx
+++ src/components/SeriesCard.tsx
@@ -1,29 +1,58 @@
-import React from "react";
-import { Pressable, StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import { Animated, Pressable, StyleSheet } from "react-native";
 import { Image } from "expo-image";
 import { Href, Link } from "expo-router";
 import { borderRadius, useThemeColor } from "./Themed";
 import { SeriesRecord } from "@/pocketbase-types";
 
 export default function SeriesCard({ series }: { series: SeriesRecord }) {
+  const scaleValue = useRef(new Animated.Value(1)).current;
+
+  const handlePressIn = () => {
+    Animated.spring(scaleValue, {
+      toValue: 1.1,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const handlePressOut = () => {
+    Animated.spring(scaleValue, {
+      toValue: 1,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <Link asChild href={series.url?.split(".org/")[1] as Href<string>}>
-      <Pressable style={{ maxWidth: 600, alignSelf: "center", width: "100%" }}>
-        {({ pressed }) => (
+    <Link asChild href={series.url?.split(".org/")[1] as Href}>
+      <Pressable
+        style={{
+          maxWidth: 600,
+          alignSelf: "center",
+          width: "100%",
+          // aspectRatio: 16 / 9,
+          borderRadius: borderRadius,
+          borderWidth: 1,
+          borderColor: useThemeColor({}, "border"),
+          overflow: "hidden",
+        }}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+      >
+        <Animated.View style={{ transform: [{ scale: scaleValue }] }}>
           <Image
             style={{
               width: "100%",
               aspectRatio: 16 / 9,
               flex: 1,
-              borderRadius: borderRadius,
-              borderWidth: pressed ? 3 : 1,
-              borderColor: useThemeColor({}, "border"),
+              // borderRadius: borderRadius,
+              // borderWidth: 1,
+              // borderColor: useThemeColor({}, "border"),
             }}
             contentFit="cover"
             source={series.imageUrl}
             transition={200}
           />
-        )}
+        </Animated.View>
       </Pressable>
     </Link>
   );
